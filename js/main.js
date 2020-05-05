c89485a0617a02e045b3d10c1272ddf8a67aa1c0
vkings1 <elmer.alluad@gmail.com>
2020-05-05 23:45:00 +0800

    Final site

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const scanForFree = document.getElementById('scanForFree');
+
+scanForFree.addEventListener('click', ()=> {
+    document.getElementById('bodyBlock').style.display="block";
+    document.getElementById('windowInstruction').style.display="block";
+
+});
+
+
