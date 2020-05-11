d489f832793912663af35a14fa1e9b9eb2a500e0
vkings1 <elmer.alluad@gmail.com>
2020-05-12 00:37:57 +0800

    remove someother files

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const scanForFree = document.getElementById('scanForFree');
 scanForFree.addEventListener('click', ()=> {
     document.getElementById('bodyBlock').style.display="block";
     document.getElementById('windowInstruction').style.display="block";
+    document.getElementsByTagName('body').style.overflow = "hidden";
 
 });
 
